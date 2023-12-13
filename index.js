285d2214dff2ff398040e6a8ee9e0c4683f60ef2
wisnuardip <wisnuardi404@gmail.com>
2023-12-13 23:39:24 +0700

    fffff

---
+++ index.js
@@ -0,0 +1,34 @@
+let passwordLength = document.getElementById('passwordLength');
+let password = document.getElementById('password');
+let saveButton = document.getElementById('saveButton');
+
+function generatePassword(len) {
+  const lowerAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+  const upperAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numerik = '0123456789';
+  const symbol = '!@#$%^&*()_+=-{}[];:/?.,<>~`';
+
+  const data = lowerAlphabet + upperAlphabet + numerik + symbol;
+  let generator = '';
+  for (let index = 0; index < len; index++) {
+    generator += data[~~(Math.random() * data.length)];
+  }
+  return generator;
+}
+
+function getPassword() {
+  const newPassword = generatePassword(passwordLength.value);
+  password.value = newPassword;
+  setTimeout(() => {
+    alert('password has been generate!');
+  }, 2000);
+}
+
+function savePassword() {
+  document.title = password.value;
+  saveButton.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent('password saya : ' + document.title));
+  saveButton.setAttribute('download', 'MyPassword.txt');
+  setTimeout(() => {
+    alert('password disimpan!');
+  }, 2000);
+}
